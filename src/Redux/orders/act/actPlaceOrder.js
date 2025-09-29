3e36f7098add135d54deee208e03c42e27f533e1
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 21:46:34 +0300

    Update actPlaceOrder.js

--- src/Redux/orders/act/actPlaceOrder.js
+++ src/Redux/orders/act/actPlaceOrder.js
@@ -2,33 +2,47 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { axiosEHandler } from "../../../utils";
 
+// رابط API ديناميكي
+const API_URL = process.env.REACT_APP_API_URL || "https://ecommerce-api-abyq.vercel.app";
 
-
-const actPlaceOrder = createAsyncThunk('orders/actPlaceOrder' , async(subtotal , thunkAPI) => {
- const { rejectWithValue, getState } = thunkAPI;
- const { cart, auth } = getState() 
-
+const actPlaceOrder = createAsyncThunk(
+  "orders/actPlaceOrder",
+  async (subtotal, thunkAPI) => {
+    const { rejectWithValue, getState } = thunkAPI;
+    const { cart, auth } = getState();
 
     const orderItems = cart.productFullInfo.map((el) => ({
       id: el.id,
       name: el.name,
       price: el.price,
       img: el.imgSrc,
-      discription:el.discription,
+      description: el.description,
       quantity: cart.items[el.id],
     }));
 
-    try{
-    const res = await axios.post("http://localhost:3001/orders", {
-        userId: auth.user?.id,
-        items: orderItems,
-        subtotal,
-      });
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+
+      const res = await axios.post(
+        `${API_URL}/orders`,
+        {
+          userId: auth.user?.id,
+          items: orderItems,
+          subtotal,
+        },
+        {
+          headers: {
+            Authorization: accessToken ? `Bearer ${accessToken}` : undefined,
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       return res.data;
-    }catch(error){
-    rejectWithValue(axiosEHandler(error))
+    } catch (error) {
+      return rejectWithValue(axiosEHandler(error));
     }
-})
+  }
+);
 
-export default  actPlaceOrder;
+export default actPlaceOrder;
