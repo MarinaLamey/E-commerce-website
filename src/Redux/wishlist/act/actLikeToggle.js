7fffe4b18c596ba95dfdeb41a1992308f71e0ed4
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 22:06:07 +0300

    Update actLikeToggle.js

--- src/Redux/wishlist/act/actLikeToggle.js
+++ src/Redux/wishlist/act/actLikeToggle.js
@@ -1,26 +1,34 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios  from "axios";
+import axios from "axios";
 import { axiosEHandler } from "../../../utils/index";
-const actLikeToggle= createAsyncThunk("wishlist/actLikeToggle" , async(id , thunkAPI)=>{
-     const { rejectWithValue, fulfillWithValue , getState} = thunkAPI;
-   const { auth } = getState()
-   const userId = auth.user.id;
-     try{
-        const userWishlist = await axios.get(`http://localhost:3001/wishlist?productId=${id}`)
-      
-       if (userWishlist.data.length > 0){
-       await axios.delete(`http://localhost:3001/wishlist/${userWishlist.data[0].id}`)
-       return{type:"remove" , id};
-       }else{
-        await axios.post("http://localhost:3001/wishlist?" , {userId , productId:id})
-          return{type:"add" , id};
-       }
-        
 
-     }catch(error){
-     return rejectWithValue( axiosEHandler(error ))
-    
-     }
-})
+// رابط API على Vercel
+const API_URL = "https://ecommerce-api-abyq.vercel.app";
+
+const actLikeToggle = createAsyncThunk(
+  "wishlist/actLikeToggle",
+  async (id, thunkAPI) => {
+    const { rejectWithValue, getState } = thunkAPI;
+    const { auth } = getState();
+    const userId = auth.user.id;
+
+    try {
+      // جلب wishlist للمنتج الحالي
+      const userWishlist = await axios.get(`${API_URL}/wishlist?productId=${id}`);
+
+      if (userWishlist.data.length > 0) {
+        // إذا موجود، نحذفه
+        await axios.delete(`${API_URL}/wishlist/${userWishlist.data[0].id}`);
+        return { type: "remove", id };
+      } else {
+        // إذا مش موجود، نضيفه
+        await axios.post(`${API_URL}/wishlist`, { userId, productId: id });
+        return { type: "add", id };
+      }
+    } catch (error) {
+      return rejectWithValue(axiosEHandler(error));
+    }
+  }
+);
 
 export default actLikeToggle;
