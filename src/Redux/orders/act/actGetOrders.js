89043ad8250bfe11fadb6f6d26f27aa752241f18
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 21:45:45 +0300

    Update actGetOrders.js

--- src/Redux/orders/act/actGetOrders.js
+++ src/Redux/orders/act/actGetOrders.js
@@ -2,17 +2,30 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { axiosEHandler } from "../../../utils";
 
+// رابط API ديناميكي
+const API_URL = process.env.REACT_APP_API_URL || "https://ecommerce-api-abyq.vercel.app";
 
-const actGetOrders = createAsyncThunk("orders/actGetOrders" , async(_ , thunkAPI) => {
-    const {rejectWithValue , fulfillWithValue ,  getState ,signal} = thunkAPI
-    const {auth} = getState()
-    try{
-     const res = await axios.get(`http://localhost:3001/orders?userId=${auth.user?.id}`);
-   
+const actGetOrders = createAsyncThunk(
+  "orders/actGetOrders",
+  async (_, thunkAPI) => {
+    const { rejectWithValue, getState, signal } = thunkAPI;
+    const { auth } = getState();
+
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+
+      const res = await axios.get(`${API_URL}/orders?userId=${auth.user?.id}`, {
+        headers: {
+          Authorization: accessToken ? `Bearer ${accessToken}` : undefined,
+        },
+        signal,
+      });
+
       return res.data;
-    }catch(error){
-     rejectWithValue(axiosEHandler(error))
+    } catch (error) {
+      return rejectWithValue(axiosEHandler(error));
     }
-})
+  }
+);
 
-export default actGetOrders
+export default actGetOrders;
