4b97fd8bf19f70c16999201a6193300f02a92ecd
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 21:38:24 +0300

    Update actUpdateProfile.js

--- src/Redux/auth/act/actUpdateProfile.js
+++ src/Redux/auth/act/actUpdateProfile.js
@@ -2,21 +2,34 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { axiosEHandler } from "../../../utils";
 
- const actUpdateProfile = createAsyncThunk(
+// رابط الـ API ديناميكي
+const API_URL = process.env.REACT_APP_API_URL || "https://ecommerce-api-abyq.vercel.app";
+
+const actUpdateProfile = createAsyncThunk(
   "auth/actUpdateProfile",
-  async( data , thunkAPI) => {
-      const { rejectWithValue , getState} = thunkAPI;
-        const { auth } = getState()
+  async (data, thunkAPI) => {
+    const { rejectWithValue, getState } = thunkAPI;
+    const { auth } = getState();
 
     try {
-      const res = await axios.patch(`http://localhost:3001/users/${auth.user.id}`, 
-    data
+      const accessToken = localStorage.getItem("accessToken"); // استخدم accessToken
+
+      const res = await axios.patch(
+        `${API_URL}/users/${auth.user.id}`,
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+        }
       );
-      return res.data; 
+
+      return res.data;
     } catch (err) {
       return rejectWithValue(axiosEHandler(err));
     }
   }
 );
 
-export default actUpdateProfile
+export default actUpdateProfile;
