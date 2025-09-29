898b7b1101273c9275d70cb0f16d22f143fe44ce
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 21:42:35 +0300

    Update actGetproducrByitems.js

--- src/Redux/cart/act/actGetproducrByitems.js
+++ src/Redux/cart/act/actGetproducrByitems.js
@@ -1,32 +1,41 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import Cartitem from "../../../componants/cartItem/Cartitem";
 import { axiosEHandler } from "../../../utils/index";
 
-const actGetproductByitems = createAsyncThunk('cart/actGetproductByitems' , async(_, thunkAPI ) => {
-    const { rejectWithValue ,  fulfillWithValue,  getState  , signal} = thunkAPI;
-    const {cart} = getState() ;
+// رابط API ديناميكي
+const API_URL = process.env.REACT_APP_API_URL || "https://ecommerce-api-abyq.vercel.app";
 
-    const itemsId = Object.keys(cart.items);    //id comes true 
+const actGetproductByitems = createAsyncThunk(
+  "cart/actGetproductByitems",
+  async (_, thunkAPI) => {
+    const { rejectWithValue, fulfillWithValue, getState, signal } = thunkAPI;
+    const { cart } = getState();
+
+    // لو items عبارة عن object keys، نجيب الـ ids
+    const itemsId = Object.keys(cart.items); 
 
     if (!itemsId.length) {
       return fulfillWithValue([]);
     }
 
-    try{
-       
-  
-         const response = await axios.get(`http://localhost:3001/products`, {signal}).then((res) => {
-           const filtered = res.data.filter(item => itemsId.includes(item.id));
-           return filtered; 
-         })
-         return response
-       
-      
-    }catch (error) {
-  return rejectWithValue( axiosEHandler(error))
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+
+      const res = await axios.get(`${API_URL}/products`, {
+        headers: { 
+          Authorization: accessToken ? `Bearer ${accessToken}` : undefined
+        },
+        signal,
+      });
+
+      // فلترة المنتجات حسب الـ ids في الكارت
+      const filtered = res.data.filter(item => itemsId.includes(String(item.id)));
+
+      return filtered;
+    } catch (error) {
+      return rejectWithValue(axiosEHandler(error));
     }
-})
+  }
+);
 
-
-export default actGetproductByitems ;
+export default actGetproductByitems;
