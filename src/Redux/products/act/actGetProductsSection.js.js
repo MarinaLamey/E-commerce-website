b6667cf3fb14de025afc9337565ce364cbb39256
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 21:53:56 +0300

    Update actGetProductsSection.js.js

--- src/Redux/products/act/actGetProductsSection.js.js
+++ src/Redux/products/act/actGetProductsSection.js.js
@@ -2,21 +2,25 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosEHandler } from "../../../utils";
 
+// رابط API ديناميكي حسب البيئة (Vercel أو local)
+const API_URL = process.env.REACT_APP_API_URL || "https://ecommerce-api-abyq.vercel.app";
 
+const actGetProductsSection = createAsyncThunk(
+  "products/actGetProductsSection",
+  async (secPrefix, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
 
+    try {
+      const res = await axios.get(`${API_URL}/products`);
+      const data = res.data;
 
-const actGetProductsSection = createAsyncThunk("products/actGetProductsSection" , async(secPrefix , thunkAPI)=> {
-  const{rejectWithValue } = thunkAPI
-
-  try{
-  const res = await axios.get(`https://68da97d423ebc87faa30ade4.mockapi.io/products?`)
-  const data = res.data;
-
-  const filtered = data.filter((p) => p["secprefix"]?.includes(secPrefix));
-  return filtered;
-  }catch(error){
-    rejectWithValue(axiosEHandler(error))
+      const filtered = data.filter((p) => p["secprefix"]?.includes(secPrefix));
+      return filtered;
+    } catch (error) {
+      return rejectWithValue(axiosEHandler(error));
+    }
   }
-})
+);
 
-export default actGetProductsSection
+export default actGetProductsSection;
+
