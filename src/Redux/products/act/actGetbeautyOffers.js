9504265bc1a34ddce306de466a831346fe7d37ac
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 21:57:30 +0300

    Update actGetbeautyOffers.js

--- src/Redux/products/act/actGetbeautyOffers.js
+++ src/Redux/products/act/actGetbeautyOffers.js
@@ -1,16 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { axiosEHandler } from "../../../utils";
-const actGetbeautyOffers =  createAsyncThunk("products/actGetbeautyOffers" , async(_ , thunkAPI) => {
-    const { rejectWithValue} =thunkAPI;
- 
-    try{
-        const res = await axios.get(`https://68da97d423ebc87faa30ade4.mockapi.io/products?categprefix=beauty&&isOffer=true`);
-       
-    return res.data
-    }catch(error){
-     return  rejectWithValue(axiosEHandler(error))
-    }
-})
 
-export default actGetbeautyOffers;  
+// رابط API على Vercel
+const API_URL = "https://ecommerce-api-abyq.vercel.app";
+
+const actGetbeautyOffers = createAsyncThunk(
+  "products/actGetbeautyOffers",
+  async (_, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+
+    try {
+      const res = await axios.get(`${API_URL}/products?categprefix=beauty&isOffer=true`);
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(axiosEHandler(error));
+    }
+  }
+);
+
+export default actGetbeautyOffers;
+
