eb2f260a11e3d277e6687aa095623f879bb1a6df
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 22:03:40 +0300

    Update actGetProduct.js

--- src/Redux/selectedProduct/act/actGetProduct.js
+++ src/Redux/selectedProduct/act/actGetProduct.js
@@ -2,15 +2,21 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosEHandler } from "../../../utils";
 
+// رابط API على Vercel
+const API_URL = "https://ecommerce-api-abyq.vercel.app";
 
-const actGetProduct = createAsyncThunk("product/actGetProduct", async(id , thunkAPI) => {
-    const {rejectWithValue} = thunkAPI
-     try{
-     const res = await axios.get(`https://68da97d423ebc87faa30ade4.mockapi.io/products?id=${id}`)
-     return res.data[0]
-     }catch(error){
-        rejectWithValue(axiosEHandler(error))
-     }
-})
+const actGetProduct = createAsyncThunk(
+  "product/actGetProduct",
+  async (id, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+
+    try {
+      const res = await axios.get(`${API_URL}/products?id=${id}`);
+      return res.data[0]; // نرجع العنصر الأول لأنه البحث برقم id
+    } catch (error) {
+      return rejectWithValue(axiosEHandler(error));
+    }
+  }
+);
 
 export default actGetProduct;
