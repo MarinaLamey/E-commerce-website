1c396cade11452c22877ba5663e750f965a58ea0
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 21:44:47 +0300

    Update actPostContact.js

--- src/Redux/customerHelp/act/actPostContact.js
+++ src/Redux/customerHelp/act/actPostContact.js
@@ -2,20 +2,24 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { axiosEHandler } from "../../../utils/index";
 
+// رابط API ديناميكي حسب البيئة
+const API_URL = process.env.REACT_APP_API_URL || "https://ecommerce-api-abyq.vercel.app";
 
- const actPostContact = createAsyncThunk(
+const actPostContact = createAsyncThunk(
   "contact/actPostContact",
   async (formData, { rejectWithValue }) => {
     try {
-
-      const { data } = await axios.post("http://localhost:3001/contacts?", {
-        firstName:formData.firstName,
-        lastName:formData.lastName,
-        email:formData.email,
-        phone:formData.phone,
-        id:formData.id,
-        message: formData.message
+      const { data } = await axios.post(`${API_URL}/contacts`, {
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        email: formData.email,
+        phone: formData.phone,
+        id: formData.id,
+        message: formData.message,
+      }, {
+        headers: { "Content-Type": "application/json" }
       });
+
       return data;
     } catch (err) {
       return rejectWithValue(axiosEHandler(err));
