2146d2f2419a6267addd80b52a3d6e9ee4c79d93
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 21:32:59 +0300

    Update actAuthLogin.js

--- src/Redux/auth/act/actAuthLogin.js
+++ src/Redux/auth/act/actAuthLogin.js
@@ -2,22 +2,38 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { axiosEHandler } from "../../../utils/index";
 
+// رابط الـ API ديناميكي
+const API_URL = process.env.REACT_APP_API_URL || "https://your-api-project.vercel.app";
 
-
-const actAuthLogin = createAsyncThunk( "auth/actAuthLogin",async (formData, thunkAPI) => {
+export const actAuthLogin = createAsyncThunk(
+  "auth/actAuthLogin",
+  async (formData, thunkAPI) => {
     const { rejectWithValue } = thunkAPI;
- 
+
     try {
-      const res = await axios.post("http://localhost:3001/login?",{email:formData.email,
-        password:formData.password
-       });
+      const res = await axios.post(
+        `${API_URL}/login`,
+        {
+          email: formData.email,
+          password: formData.password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      // حفظ accessToken بدل token
+      if (res.data.accessToken) {
+        localStorage.setItem("accessToken", res.data.accessToken);
+      }
 
       return res.data;
-
     } catch (error) {
       return rejectWithValue(axiosEHandler(error));
     }
   }
 );
 
-export default actAuthLogin
+export default actAuthLogin;
