32d21c8836e2e1e2df8c729ffe41721691ab3784
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 22:02:37 +0300

    Update actGetSearchproducts.js

--- src/Redux/searchProducts/act/actGetSearchproducts.js
+++ src/Redux/searchProducts/act/actGetSearchproducts.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import { createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 import { axiosEHandler } from "../../../utils";
 
-const  actGetSearchproducts = createAsyncThunk("search/actGetSearchproducts" , async(searchTerm , thunkAPI)=> {
- 
-  const { rejectWithValue} =thunkAPI;
-    try{
-     const res = await axios.get(`https://68da97d423ebc87faa30ade4.mockapi.io/products?q=${searchTerm}`);
-    return res.data;
-    }catch(error){
-    return rejectWithValue(axiosEHandler(error))
-    }
-})
+// رابط API على Vercel
+const API_URL = "https://ecommerce-api-abyq.vercel.app";
 
-export default actGetSearchproducts
+const actGetSearchproducts = createAsyncThunk(
+  "search/actGetSearchproducts",
+  async (searchTerm, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+
+    try {
+      const res = await axios.get(`${API_URL}/products?q=${searchTerm}`);
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(axiosEHandler(error));
+    }
+  }
+);
+
+export default actGetSearchproducts;
