15c66ebdd7776f69f08230fc8b534e95d7cb5b9d
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 21:43:35 +0300

    Update actGetcategories.js

--- src/Redux/categories/act/actGetcategories.js
+++ src/Redux/categories/act/actGetcategories.js
@@ -2,22 +2,20 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { axiosEHandler } from "../../../utils/index";
 
+// رابط API ديناميكي (ممكن تغيّريه بسهولة في المستقبل)
+const API_URL = process.env.REACT_APP_CATEGORIES_API || "https://68da97d423ebc87faa30ade4.mockapi.io";
 
 const actGetcategories = createAsyncThunk(
   "categories/actGetcategories",
   async (_, thunkAPI) => {
-    const { rejectWithValue , signal } = thunkAPI;
+    const { rejectWithValue, signal } = thunkAPI;
     try {
-      const response = await axios.get(
-        "https://68da97d423ebc87faa30ade4.mockapi.io/categories" , {
-          signal
-        }
-      );
-      return response.data;
+      const res = await axios.get(`${API_URL}/categories`, { signal });
+      return res.data;
     } catch (error) {
-      return rejectWithValue( axiosEHandler(error ))
+      return rejectWithValue(axiosEHandler(error));
     }
   }
 );
 
-export default actGetcategories
+export default actGetcategories;
