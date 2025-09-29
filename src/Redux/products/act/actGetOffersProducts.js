5cbd3c2f75c4e285b02df028cba171679d607f3b
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 21:47:23 +0300

    Update actGetOffersProducts.js

--- src/Redux/products/act/actGetOffersProducts.js
+++ src/Redux/products/act/actGetOffersProducts.js
@@ -2,19 +2,21 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { axiosEHandler } from "../../../utils";
 
+// رابط API ديناميكي (لو حبيتي تغيّريه مستقبلاً)
+const API_URL = process.env.REACT_APP_PRODUCTS_API || "https://68da97d423ebc87faa30ade4.mockapi.io";
 
+const actGetOffersProducts = createAsyncThunk(
+  "products/actGetOffersProducts",
+  async (_, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
 
-
-const actGetOffersProducts = createAsyncThunk("products/actGetOffersProducts" , async(_ , thunkAPI) => {
-  const{rejectWithValue } = thunkAPI
-
-  try{
-  const res = await axios.get(`https://68da97d423ebc87faa30ade4.mockapi.io/products?isOffer=true`)
- 
-   return res.data
-  }catch(error){
-    rejectWithValue(axiosEHandler(error))
+    try {
+      const res = await axios.get(`${API_URL}/products?isOffer=true`);
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(axiosEHandler(error));
+    }
   }
-})
+);
 
-export default actGetOffersProducts
+export default actGetOffersProducts;
