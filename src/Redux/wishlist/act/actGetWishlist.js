dd88fa8cd0775eed725ee15329b70c2684a190cd
MarinaLamey <108277791+MarinaLamey@users.noreply.github.com>
2025-09-29 22:04:53 +0300

    Update actGetWishlist.js

--- src/Redux/wishlist/act/actGetWishlist.js
+++ src/Redux/wishlist/act/actGetWishlist.js
@@ -2,32 +2,38 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { axiosEHandler } from "../../../utils/index";
 
-const actGetWishlistProduct = createAsyncThunk('wishlist/actGetWishlistProduct',async(dataType,thunkAPI)=> {
-     const { rejectWithValue , signal , getState} = thunkAPI;
-    const { auth } = getState()
-     try {
-   const userWishlist = await axios.get(`http://localhost:3001/wishlist?userId=${auth.user.id}`)
-   
-       if (!userWishlist.data.length) {
-        return { data:[], dataType: "empty" };
+// رابط API على Vercel
+const API_URL = "https://ecommerce-api-abyq.vercel.app";
+
+const actGetWishlistProduct = createAsyncThunk(
+  "wishlist/actGetWishlistProduct",
+  async (dataType, thunkAPI) => {
+    const { rejectWithValue, getState, signal } = thunkAPI;
+    const { auth } = getState();
+
+    try {
+      // جلب wishlist للمستخدم
+      const userWishlist = await axios.get(`${API_URL}/wishlist?userId=${auth.user.id}`, { signal });
+
+      if (!userWishlist.data.length) {
+        return { data: [], dataType: "empty" };
       }
 
-     if (dataType === "ProductIds") {
-    
-     const productId = userWishlist.data.map(item => item.productId)
-      return { data: productId, dataType: "ProductsIds" };
-       }else { 
-const ids = userWishlist.data.map(item => item.productId).join("&id=");
-const response = await axios.get(`http://localhost:3001/products?id=${ids}`);
-   
-      return { data: response.data, dataType: "ProductsFullInfo" };
-        }
+      if (dataType === "ProductIds") {
+        const productId = userWishlist.data.map((item) => item.productId);
+        return { data: productId, dataType: "ProductsIds" };
+      } else {
+        // جلب معلومات كاملة عن المنتجات
+        const ids = userWishlist.data.map((item) => item.productId).join("&id=");
+        const response = await axios.get(`${API_URL}/products?id=${ids}`, { signal });
 
+        return { data: response.data, dataType: "ProductsFullInfo" };
+      }
+    } catch (error) {
+      return rejectWithValue(axiosEHandler(error));
+    }
+  }
+);
 
-  
-     }catch (error){
-     return  rejectWithValue( axiosEHandler(error ))
-     }
-})
+export default actGetWishlistProduct;
 
-export default actGetWishlistProduct 
